0ee6c54977f141f4f143007d5ad466b05b900576
Autumn Edwards <autumnedwards2@icloud.com>
2022-02-25 18:43:19 -0500

    Update project0.s
    i have to multiply the integer i get when i divided in the previous step by 11.
    then i subtracted the value obtained in the previous step from my original id number.
    this will result in the modulus.

--- project0.s
+++ project0.s
@@ -9,3 +9,7 @@ li, $t2, 11 #loading the number 11 to the register t2
 #begin the process of finding 2992633 % 11
 div $t1,$t2 #dividing my id number by the number 11
 mflo $t3 #storing the whole number portion to the register t3
+mult $t2, $t3 #multiply the whole number portion by 11
+mflo $t4 #storing the low 32 bits to register t2
+sub $t5, $t1, $t4 # 2992633 - 2992627 (final step of finding the mod) and storing it in register t3
+#the process of finding the modulo which is the number of times the program runs is completed
