8eede75a3184c1b56585517fae9bb27980dcb669
Autumn Edwards <autumnedwards2@icloud.com>
2022-02-25 18:50:13 -0500

    Update project0.s
    the next line is a system call that will print the memory stored in the .data.
    the addi line increments the variable stored in t0 so the loop will not continue forever and ever
    finally the code jumps back to the start of the loop and will continue to do this until it exits once the integer is greater than or equal to the mod value calculated

--- project0.s
+++ project0.s
@@ -16,4 +16,9 @@ sub $t5, $t1, $t4 # 2992633 - 2992627 (final step of finding the mod) and storin
 
 startloop:
   	bgez $t0, $t5, exitloop #when t0 is greater than the modulus stop the loop
-  	la $a0, hello #loading the address hello(which is the string that we implemented 
+  	la $a0, hello #loading the address hello(which is the string that we implemented in the data section)
+	  li $v0, 4 #through the system call 4 the address will print
+	  syscall #this just means system call
+	  addi $t0,$t0, 1 #incrementing the variable stored in register t0 
+	  j startloop
+exitloop:
