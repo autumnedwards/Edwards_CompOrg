707528f1742d191fa401ce310b735573cddabc89
Autumn Edwards <autumnedwards2@icloud.com>
2022-02-25 18:40:47 -0500

    Update project0.s
    in order to find the modulus (my id number %11) i have to use the divide and mflo function to divide my id number by 11 and store the lower answer in register 3

--- project0.s
+++ project0.s
@@ -5,3 +5,7 @@ hello: .asciiz "Hello, @02992633\n" #inputting the string hello, @student id in 
 move $t0,$0 #assigning the value 0 to register t0
 li, $t1,2992633 #loading my id to the register t1
 li, $t2, 11 #loading the number 11 to the register t2
+
+#begin the process of finding 2992633 % 11
+div $t1,$t2 #dividing my id number by the number 11
+mflo $t3 #storing the whole number portion to the register t3
