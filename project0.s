2eb6941ccb1d21669ad779ef375b4c9add61db98
Autumn Edwards <autumnedwards2@icloud.com>
2022-02-25 18:52:53 -0500

    Update project0.s
    i changed the loop from bgez to beq (branch on equal) because the loop was running one too many times

--- project0.s
+++ project0.s
@@ -16,7 +16,7 @@ sub $t5, $t1, $t4 # 2992633 - 2992627 (final step of finding the mod) and storin
 #the process of finding the modulo which is the number of times the program runs is completed
 
 startloop:
-  	bgez $t0, $t5, exitloop #when t0 is greater than the modulus stop the loop
+  	beq $t0, $t5, exitloop #when t0 is greater than the modulus stop the loop
   	la $a0, hello #loading the address hello(which is the string that we implemented in the data section)
 	  li $v0, 4 #through the system call 4 the address will print
 	  syscall #this just means system call
