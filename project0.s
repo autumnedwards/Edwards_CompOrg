71870e86ae5d6e35ef80fe68942e2f4036745c9f
Autumn Edwards <autumnedwards2@icloud.com>
2022-02-25 11:29:22 -0500

    Update project0.s

--- project0.s
+++ project0.s
@@ -1,2 +1,8 @@
-.data
+.data #this section is where all data is stored to the memory
 hello: .asciiz "Hello, @02992633" #inputting the string hello, @student id in the memory
+
+.text #this section is where the MIPS code is used 
+
+move $t0,$0 #assigning the value 0 to register t0
+li, $t1,2992633 #loading my id to the register t1
+li, $t2, 11 #loading the number 11 to the register t2
