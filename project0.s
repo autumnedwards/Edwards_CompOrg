3f8b1733520b09fe06e69aba62c4b89719b4a8b0
Autumn Edwards <autumnedwards2@icloud.com>
2022-02-25 11:32:15 -0500

    Update project0.s

--- project0.s
+++ project0.s
@@ -11,3 +11,5 @@ li, $t2, 11 #loading the number 11 to the register t2
 div $t1,$t2 #dividing my id number by the number 11
 mflo $t3 #storing the whole number portion to the register t3
 mult $t2, $t3 #multiply the whole number portion by 11
+mflo $t2 #storing the low 32 bits to register t2
+sub $t3, $t1-$t2 # 2992633 - 2992627 (final step of finding the mod) and storing it in register t3
