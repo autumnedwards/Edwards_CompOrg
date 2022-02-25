37384156fc2e99469e7d1a575d61cffbf7c501a6
Autumn Edwards <autumnedwards2@icloud.com>
2022-02-25 16:54:48 -0500

    Update project0.s

--- project0.s
+++ project0.s
@@ -11,12 +11,12 @@ li, $t2, 11 #loading the number 11 to the register t2
 div $t1,$t2 #dividing my id number by the number 11
 mflo $t3 #storing the whole number portion to the register t3
 mult $t2, $t3 #multiply the whole number portion by 11
-mflo $t2 #storing the low 32 bits to register t2
-sub $t3, $t1, $t2 # 2992633 - 2992627 (final step of finding the mod) and storing it in register t3
+mflo $t4 #storing the low 32 bits to register t2
+sub $t5, $t1, $t4 # 2992633 - 2992627 (final step of finding the mod) and storing it in register t3
 #the process of finding the modulo which is the number of times the program runs is completed
 
 startloop:
-  	bgt $t0, $t3, exitloop #when t0 is greater than the modulus stop the loop
+  	bgt $t0, $t5, exitloop #when t0 is greater than the modulus stop the loop
   	la $a0, hello #loading the address hello(which is the string that we implemented in the data section)
 	li $v0, 4 #through the system call 4 the address will print
 	syscall #this just means system call
