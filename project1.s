75c11f673189fed3e2397d99682c17cc11140cd4
Autumn Edwards <autumnedwards2@icloud.com>
2022-03-18 01:15:23 -0400

    Update project1.s
    adding .space 10 in the data section because this specifies that there are 10 bytes set aside, also added comments to the lines where the MIPS code was reading the string

--- project1.s
+++ project1.s
@@ -1,10 +1,10 @@
 .data
   instruct: .asciiz "Enter 10 characters" #instructing the user to enter a string of 10 characters 
-  
+  .space 10 #allocating 10 bytes for the string b/c 1 character = 1 byte
 .text
 main:
 
-addi, $v0, $0, 8
-la $a0, instruct
-li $a1, 10
-
+addi, $v0, $0, 8 #system call code for a reading a string in MIPS is 8
+la $a0, instruct #a0 register set to the location in memory to which the computer will record the input
+li $a1, 10 # setting the max number of characters that should be read in 
+syscall 
